eaec45e5ccc3b408a5e08992d3a79752e7ea8020
Ankit <ankit.95sharma@gmail.com>
2022-05-24 23:29:31 +0530

    add static files css/js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const port = 8000;
 const app = express();
 
+//for static files like css, js, etc
+app.use(express.static("./assets"));
+
 //use express router
 app.use("/", require("./routes/"));
 
